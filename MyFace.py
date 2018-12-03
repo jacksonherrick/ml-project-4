ca824835ada37d7e6b6327496144d88b75bc60d9
dfinazzo20 <dfinazzo20@amherst.edu>
2018-12-02 20:36:19 -0500

    data

--- MyFace.py
+++ MyFace.py
@@ -1,3 +1,18 @@
-import matplotlib
-import numpy
+import matplotlib as plt
+import numpy as np
+import sklearn as sk
+
+def parse_data():
+
+    pass
+
+def parse_friends():
+
+    pass
+
+if __name__ == "__main__":
+
+    pass
+
+
 
