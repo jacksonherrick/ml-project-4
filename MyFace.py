9dc163f4a9153ad9a54f2dd681cccad111c657bc
jacksonherrick <jacksonherrick8@gmail.com>
2018-12-03 22:15:59 -0500

    data-parsing
    Basic data parsing should work now so we can try actually running some tests. Still have to write unit tests for this.

--- MyFace.py
+++ MyFace.py
@@ -4,38 +4,46 @@ import sklearn as sk
 
 def parse_data():
 
-    pass
+    # read data from source files
+    posts_train = np.genfromtxt("posts_train.txt", delimiter = ",", skip_header=1)
+    posts_test = np.genfromtxt("posts_test.txt", delimiter = ",", skip_header=1)
+    friends = np.genfromtxt("small_graph.txt")
 
-def parse_friends():
+    return posts_train, posts_test, friends
 
-    pass
 
-def create_features(data, friends):
+def create_features(posts, friends, locations):
     pass
+    # we should include some features from the posts of the friends of each person. Number, avg location, popularity, etc
 
-def predict_lat(features):
-    pass
-    #add predicted latitude to longitude features
+def setup_data():
 
-def predict_long(features):
-    pass
+    # read the data in from the source files
+    posts_train, posts_test, friends = parse_data()
+    # locations = Ids with lat and lon columns from posts_train
+    locations = np.concatenate((posts_train[:,0:1], posts_train[:, 4:6]), 1)
+    # removes lat and lon from posts_train so it looks like test data
+    posts_train = np.concatenate((posts_train[:,0:4], posts_train[:, 6:]), 1)
 
-def parse_test_data():
-    pass
+    # extract features from post and friend data
+    features_train = create_features(posts_train, friends, locations)
+    features_test = create_features(posts_test, friends, locations)
+    # strip the Ids off of the training targets so they are just lat and lon
+    targets_train = locations[:, 1:]
+
+    return features_train, features_test, targets_train
 
 if __name__ == "__main__":
 
+    # parse our data into features and targets
+    features_train, features_test, targets_train = setup_data()
 
-    pass
-    data, targets = parse_data()
-    friends = parse_friends()
-    features = create_features(data, friends)
-    test_data = parse_test_data()
-
+    # train the learner
     learner = sk.neighbors.KNeighborsRegressor()
-    learner.fit(features, targets)
+    learner.fit(features_train, targets_train)
 
-    predictions = (learner.predict(test_data))
+    #predict
+    predictions = (learner.predict(features_test))
 
 
 
